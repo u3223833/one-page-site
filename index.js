5ec0064400d073e2f2e0109a2c8c1e5f52d100d2
u3223833 <112055166+u3223833@users.noreply.github.com>
2022-09-26 16:11:38 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-function update(e){
-  var x = e.clientX || e.touches[0].clientX
-  var y = e.clientY || e.touches[0].clientY
-
-  document.documentElement.style.setProperty('--cursorX', x + 'px')
-  document.documentElement.style.setProperty('--cursorY', y + 'px')
+function trackMouse(event) {
+  document.documentElement.style.setProperty(
+    '--cursorXPos',
+    `${event.clientX}px`
+  )
+  document.documentElement.style.setProperty(
+    '--cursorYPos',
+    `${event.clientY}px`
+  )
 }
-
-document.addEventListener('mousemove',update)
-document.addEventListener('touchmove',update)
+​
+document.addEventListener('mousemove', trackMouse)
