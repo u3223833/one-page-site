6e5014dc5a7eb6dcf7a728c41e327abf499a41c9
u3223833 <112055166+u3223833@users.noreply.github.com>
2022-09-26 16:43:17 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-const spotlight = document.getElementById("spotlight");
-const spotlight_child = document.getElementById("spotlight-child");
-const main = document.getElementById("main");
-main.addEventListener("mousemove", moveSpotlight);
-main.addEventListener("touchmove", moveSpotlight);
-function moveSpotlight(e) {
-  let pos, x, y;
-  e.preventDefault();
-  x = e.clientX - 300;
-  y = e.clientY - 350;
-  spotlight.style.left = x + "px";
-  spotlight.style.top = y + "px";
-  spotlight_child.style.left = x + "px";
-  spotlight_child.style.top = y + "px";
-}
+
 
