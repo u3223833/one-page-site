aa3eeab9fc45912293c96247cc48920575bd98d1
u3223833 <112055166+u3223833@users.noreply.github.com>
2022-10-23 22:53:08 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 //typewritter effect
+'use strict';
 
-var i = 0;
-var txt = 'Nothing beats a good ghost story. Behind their facades hides a skeleton—or two. The creek of a floorboard or the flicker of a light is just the beginning of the frightening tales these locations have to tell. If you are a fiend for chilling stories that send goosebumps up your arm you are in the right place'; /* The text */
+function typeWriter(el) {
+    const textArray = el.innerHTML.split('');
+    el.innerHTML = '';
+    textArray.forEach((letter, i) =>
+        setTimeout(() => (el.innerHTML += letter), 95 * i)
+    );
 
-var speed = 50; /* The speed/duration of the effect in milliseconds */
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("typo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+    setInterval(() => typeWriter(el), 8000);
 }
+
+typeWriter(elementEl);
