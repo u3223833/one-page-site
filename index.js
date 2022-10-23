9b344bfda07308246868176293f20e353a45009f
u3223833 <112055166+u3223833@users.noreply.github.com>
2022-10-23 22:18:34 +1100

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function update(e){
   var x = e.clientX || e.touches[0].clientX
   var y = e.clientY || e.touches[0].clientY
 
-  document.documentElement.style.setProperty('--cursorX', x + 'px')
-  document.documentElement.style.setProperty('--cursorY', y + 'px')
+  document.style.setProperty('--cursorX', x + 'px')
+  document.style.setProperty('--cursorY', y + 'px')
 }
 
 document.addEventListener('mousemove',update)
