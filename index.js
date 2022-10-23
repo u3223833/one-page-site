5e3dae09f18837a0d21027fc973b84d566fb080c
u3223833 <112055166+u3223833@users.noreply.github.com>
2022-10-23 22:36:54 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 //typewritter effect
 
 var i = 0;
-var txt = 'Lorem ipsum dummy text blabla.';
+var txt = 'Nothing beats a good ghost story. Behind their facades hides a skeleton—or two. The creek of a floorboard or the flicker of a light is just the beginning of the frightening tales these locations have to tell. If you are a fiend for chilling stories that send goosebumps up your arm you are in the right place';
+
 var speed = 50;
 
 function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
+    document.getElementById("typo").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
