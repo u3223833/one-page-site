6268317ef8a780c09b9aaafb9873da2d47291d30
u3223833 <112055166+u3223833@users.noreply.github.com>
2022-10-23 20:40:22 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
 
-//typewritter effect
-'use strict';
-
-function typeWriter(el) {
-    const textArray = el.innerHTML.split('');
-    el.innerHTML = '';
-    textArray.forEach((letter, i) =>
-        setTimeout(() => (el.innerHTML += letter), 95 * i)
-    );
-    setInterval(() => typeWriter(el), 8000);
-}
-
-typeWriter(elementEl);
