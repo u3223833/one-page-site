bc23530448da2bca130b5b89e0fce6803021fad1
u3223833 <112055166+u3223833@users.noreply.github.com>
2022-10-23 20:31:27 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+//click me 
 $(document).ready(function(){
   $("#hover-area").click(function(){
     $("#reveal").slideToggle(300);
   });
 });
+
+//typewritter effect
+'use strict';
+
+function typeWriter(el) {
+    const textArray = el.innerHTML.split('');
+    el.innerHTML = '';
+    textArray.forEach((letter, i) =>
+        setTimeout(() => (el.innerHTML += letter), 95 * i)
+    );
+    setInterval(() => typeWriter(el), 8000);
+}
+
+typeWriter(elementEl);
