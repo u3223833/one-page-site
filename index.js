c7ef1460f6b6ebec3170c781075894cfa2a2cbba
u3223833 <112055166+u3223833@users.noreply.github.com>
2022-10-23 22:40:17 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //typewritter effect
 
 var i = 0;
-var txt = 'Nothing beats a good ghost story. Behind their facades hides a skeleton—or two. The creek of a floorboard or the flicker of a light is just the beginning of the frightening tales these locations have to tell. If you are a fiend for chilling stories that send goosebumps up your arm you are in the right place';
+var txt = 'Nothing beats a good ghost story. Behind their facades hides a skeleton—or two. The creek of a floorboard or the flicker of a light is just the beginning of the frightening tales these locations have to tell. If you are a fiend for chilling stories that send goosebumps up your arm you are in the right place'; /* The text */
 
-var speed = 50;
+var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
   if (i < txt.length) {
