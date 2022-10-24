7c4f75f09b0b96f2337e78689dfa240603b81a7a
u3223833 <112055166+u3223833@users.noreply.github.com>
2022-10-24 19:51:46 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-//intro text
+  //type writter effect
 
-/*console.clear();
-function typingEffect(element,speed){
-  let text=element.innerHTML;
-  element.innerHTML="";
- var i=0;
-  var timer=setInterval(function(){
-    if(i<text.length){
-      element.append(text.charAt(i))
-      i++;
-    }else{
-      clearInterval(timer);
-    }
-  },speed)
-  
-}
-
-const p1=document.querySelector('p1');
-typingEffect(p1,150); */
-
-//type writter effect
 let sentences = document.querySelectorAll('.sentence');
 let characterCount = 0;
 
@@ -28,9 +8,12 @@ for (let i = 0; i < sentences.length; i++) {
   let newContent = '';
 
   // go through all characters of the sentence
+  
   for (let j = 0; j < sentence.textContent.length; j++) {
     let substring = sentence.textContent.substr(j, 1);
+    
     // if we have a character, wrap it
+    
     if (substring !== " ") {
       newContent += `<span style="--animation-order: ${characterCount};">${substring}</span>`;
     } else {
