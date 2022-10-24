ad5ff5eac9a521abe5691cd3ac1f1424e461f2aa
u3223833 <112055166+u3223833@users.noreply.github.com>
2022-10-24 22:58:47 +1100

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ let mouseX = 0;
 let mouseY = 0;
 let flashlight = document.getElementById("flashlight");
 
-//Detect touch device
 const isTouchDevice = () => {
   try {
-    //We try to create TouchEvent(it would fail for desktops and throw error)
+
     document.createEvent("TouchEvent");
     return true;
   } catch (e) {
