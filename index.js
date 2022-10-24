b37951911d5479d1ad876a4df64b1ad2cf1dfe17
u3223833 <112055166+u3223833@users.noreply.github.com>
2022-10-24 22:20:58 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-//"click" to reveal image
+/*flashlight effect*/
 
-function toggleImage() {
-  var image = document.getElementById('image');
-  image.classList.toggle('hidden');
+let mouseX = 0;
+let mouseY = 0;
+let flashlight = document.getElementById("flashlight");
+
+//Detect touch device
+const isTouchDevice = () => {
+  try {
+    //We try to create TouchEvent(it would fail for desktops and throw error)
+    document.createEvent("TouchEvent");
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+function getMousePosition(e) {
+  mouseX = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
+  mouseY = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
+
+  flashlight.style.setProperty("--Xpos", mouseX + "px");
+  flashlight.style.setProperty("--Ypos", mouseY + "px");
 }
 
-var button = document.getElementById('button')
-button.addEventListener('click', toggleImage)
+document.addEventListener("mousemove", getMousePosition);
+document.addEventListener("touchmove", getMousePosition);
