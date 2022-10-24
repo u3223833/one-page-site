6ca37cc5bcdff11152690a1bffe9cb02eba416b8
u3223833 <112055166+u3223833@users.noreply.github.com>
2022-10-24 19:11:06 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //intro text
 
-console.clear();
+/*console.clear();
 function typingEffect(element,speed){
   let text=element.innerHTML;
   element.innerHTML="";
@@ -17,6 +17,26 @@ function typingEffect(element,speed){
 }
 
 const p1=document.querySelector('p1');
-typingEffect(p1,150);
+typingEffect(p1,150); */
 
+//type writter effect
+let sentences = document.querySelectorAll('.sentence');
+let characterCount = 0;
 
+for (let i = 0; i < sentences.length; i++) {
+  let sentence = sentences[i];
+  let newContent = '';
+
+  // go through all characters of the sentence
+  for (let j = 0; j < sentence.textContent.length; j++) {
+    let substring = sentence.textContent.substr(j, 1);
+    // if we have a character, wrap it
+    if (substring !== " ") {
+      newContent += `<span style="--animation-order: ${characterCount};">${substring}</span>`;
+    } else {
+      newContent += substring;
+    }
+    characterCount++;
+  }
+  sentence.innerHTML = newContent;
+}
