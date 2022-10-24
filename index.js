87e07a001425a2b96640a038e0468223c31b0d2f
u3223833 <112055166+u3223833@users.noreply.github.com>
2022-10-24 19:01:42 +1100

    Update index.js

--- index.js
+++ index.js
@@ -20,12 +20,3 @@ const p1=document.querySelector('p1');
 typingEffect(p1,150);
 
 
-//questions 
-
-var string = "COULD THESE SIGHTINGS BE CONNECTED TO THESE TRAGIC EVENTS? OR IS IT SIMPLY JUST AN URBAN LEGEND?";
-var str = string.split("");
-var el = document.getElementById('str');
-(function animate() {
-str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
-var running = setTimeout(animate, 90);
-})();
