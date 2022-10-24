129e1b37b1b57ab0c0e28f5240b8c3597e2a3f3b
u3223833 <112055166+u3223833@users.noreply.github.com>
2022-10-24 16:39:05 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+console.clear();
+function typingEffect(element,speed){
+  let text=element.innerHTML;
+  element.innerHTML="";
+ var i=0;
+  var timer=setInterval(function(){
+    if(i<text.length){
+      element.append(text.charAt(i))
+      i++;
+    }else{
+      clearInterval(timer);
+    }
+  },speed)
+  
+}
+
+
+
+const p1=document.querySelector('p1');
+typingEffect(p1,150);
+
 
