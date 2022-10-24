1d2353a711b0d311173788dc630fa3bd990803fe
u3223833 <112055166+u3223833@users.noreply.github.com>
2022-10-24 21:01:05 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+//"click" to reveal image
 
+function toggleImage() {
+  var image = document.getElementById('image');
+  image.classList.toggle('hidden');
+}
+
+var button = document.getElementById('button')
+button.addEventListener('click', toggleImage)
