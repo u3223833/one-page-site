9dd9094914b3b300fa36ac58c58cfd2d19be74f9
u3223833 <112055166+u3223833@users.noreply.github.com>
2022-10-24 17:46:34 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//intro text
+
 console.clear();
 function typingEffect(element,speed){
   let text=element.innerHTML;
@@ -14,9 +16,16 @@ function typingEffect(element,speed){
   
 }
 
-
-
 const p1=document.querySelector('p1');
 typingEffect(p1,150);
 
 
+//questions 
+
+var string = "COULD THESE SIGHTINGS BE CONNECTED TO THESE TRAGIC EVENTS? OR IS IT SIMPLY JUST AN URBAN LEGEND?";
+var str = string.split("");
+var el = document.getElementById('str');
+(function animate() {
+str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
+var running = setTimeout(animate, 90);
+})();
