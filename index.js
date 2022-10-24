49113b5907219c1286b61957a13296a95b3f68bc
u3223833 <112055166+u3223833@users.noreply.github.com>
2022-10-24 14:57:43 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-//flashligh effect
 
-function move(e){
-        var x = e.clientX || e.touches[0].clientX
-        var y = e.clientY || e.touches[0].clientY
-
-        document.documentElement.style.setProperty('--cursorX', x + 'px')
-        document.documentElement.style.setProperty('--cursorY', y + 'px')
-        }
-        document.addEventListener('mousemove',move)
-        document.addEventListener('touchmove',move)
