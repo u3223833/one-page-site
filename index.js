1304bd5d1dfda8bfdb466e7ccc332dc076b23581
u3223833 <112055166+u3223833@users.noreply.github.com>
2022-10-24 23:23:02 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 /*flashlight effect*/
 
+let flashlight = document.getElementById("flashlight");
+function update(e) {
 let mouseX = 0;
 let mouseY = 0;
-let flashlight = document.getElementById("flashlight");
 
-const isTouchDevice = () => {
-  try {
-
-    document.createEvent("TouchEvent");
-    return true;
-  } catch (e) {
-    return false;
-  }
-};
-
-function getMousePosition(e) {
-  mouseX = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
-  mouseY = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
 
   flashlight.style.setProperty("--Xpos", mouseX + "px");
   flashlight.style.setProperty("--Ypos", mouseY + "px");
@@ -24,3 +12,6 @@ function getMousePosition(e) {
 
 document.addEventListener("mousemove", getMousePosition);
 document.addEventListener("touchmove", getMousePosition);
+
+document.addEventListener('mousemove', update)
+document.addEventListener('touchmove', update)
